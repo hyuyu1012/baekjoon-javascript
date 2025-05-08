1e0107ff95811165bf90e06190da33e51cf2c0e5
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-05-09 08:00:44 +0900

    [Bronze II] Title: 문자열 반복, Time: 92 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const init = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const T = init[0];
+
+for (let i = 1; i <= T; i++){
+  let result = '';
+  let testcase = init[i].split(' ');
+  
+  let r = Number(testcase[0])
+  let c_array = testcase[1].split('');
+
+  c_array.forEach(str => {
+    for (let j = 0; j < r; j++){
+      result = result + str;
+    }
+  })
+  
+  console.log(result);
+
+}
