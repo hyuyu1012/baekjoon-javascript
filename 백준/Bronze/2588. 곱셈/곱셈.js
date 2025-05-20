06b5740b7fd6ee6621730708ff9ca1c18db1c21b
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-05-21 08:57:02 +0900

    [Bronze III] Title: 곱셈, Time: 136 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const init = fs.readFileSync("/dev/stdin").toString().trim().split("\n")
+
+
+const n1 = Number(init[0]);
+
+const n2Array = init[1].split("").map(Number);
+for (let i = n2Array.length-1; i >=0; i--) {
+  console.log(n1 * n2Array[i]);
+}
+console.log(n1 * Number(init[1]));
+
