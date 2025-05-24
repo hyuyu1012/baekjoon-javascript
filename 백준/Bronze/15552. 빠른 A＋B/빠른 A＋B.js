a7dd956caa704a3e73556e6d62d2c0dd82cdbdb4
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-05-24 14:57:55 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1336 ms, Memory: 180784 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const init = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const t = Number(init[0]);
+let result = '';
+
+for(let i = 1; i<=t; i++){
+  const init2 = init[i].split(" ").map(Number);
+  result = result + (init2[0] + init2[1]) + "\n";
+}
+
+console.log(result.trim());
