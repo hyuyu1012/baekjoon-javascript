7a3e0eaf08150695ee8f2af723e2ddb0d7b8293e
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-05-24 14:16:44 +0900

    [Bronze IV] Title: 영수증, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const init  = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+// 영수증에 적힌 금액
+let x = init[0];
+// 종류 개수
+let n = init[1];
+
+for(let i = 2; i<= init.length-1; i++) {
+  const list = init[i].split(" ").map(Number);
+  const price = list[0];
+  const count = list[1];
+  x = x - price * count;
+}
+
+if (x === 0) {
+  console.log("Yes");
+}
+else {
+  console.log("No")
+}
