b3a535c4b10b74b7c1038505a72d7b53486a6501
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-05-31 15:56:42 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 112 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const str = fs.readFileSync(0).toString().trim().split("");
+
+const result = new Array(26).fill(0);
+
+str.forEach(s => {
+  const index = s.charCodeAt() -97 ;
+  result[index] = result[index] + 1; 
+})
+
+console.log(result.join(" "));
