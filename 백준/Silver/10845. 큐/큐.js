43c818e49c7efe25fb5d70c13c5891198ef325f3
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-06-12 12:50:10 +0900

    [Silver IV] Title: 큐, Time: 108 ms, Memory: 12080 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const init = fs.readFileSync(0).toString().trim().split("\n");
+const queue = [];
+const n = init[0];
+const result = [];
+for (let i = 1; i<=n; i++) {
+  const temp = init[i].split(" ");
+  const cmd = temp[0];
+  
+  if(cmd ==="push"){
+    queue.push(temp[1])
+  }
+  else if(cmd ==="pop"){
+    if(queue.length===0) {
+      result.push(-1);
+    }
+    else {
+      result.push(queue.shift());
+    }
+  }
+  else if (cmd === "size") {
+    result.push(queue.length);
+  }
+  else if (cmd === "empty") {
+    if (queue.length === 0) {
+      result.push(1);
+    } 
+    else {
+      result.push(0);
+    }
+  }
+  else if (cmd === "front") {
+    if (queue.length === 0){
+      result.push(-1)
+    }
+    else {
+    result.push(queue[0]);
+    }
+  }
+  else if (cmd === "back") {
+    if (queue.length === 0){
+      result.push(-1)
+    }
+    else {
+    result.push(queue[queue.length -1 ]);
+    }
+  }
+
+}
+console.log(result.join("\n"));
+
+
