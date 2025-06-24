f4825c33a57c9226ed3685276913f260a67b8d55
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-06-25 08:50:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 364 ms, Memory: 43516 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const init = fs.readFileSync(0).toString().trim().split("\n").map((line) => line.split(" "));
+const n = Number(init.shift());
+const result = []
+
+init.sort((a,b) => {
+  return (Number(a[0]) - Number(b[0])); 
+});
+
+
+console.log(init.map(arr => arr.join(" ")).join("\n"));
