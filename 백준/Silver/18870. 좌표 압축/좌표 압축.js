280c7b89e03beaf05ec550a5c4739d56bd4aa730
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-07-05 15:34:35 +0900

    [Silver II] Title: 좌표 압축, Time: 1956 ms, Memory: 361528 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const init = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(init[0]);
+const arr = init[1].split(" ").map(Number);
+const result = [];
+
+const orderObj = Object.create(null);
+const sortArray = Array.from(new Set(arr)).sort((a,b) => {return a - b});
+sortArray.forEach((num, index) => {
+  if(num in orderObj) {
+
+  }
+  else {
+    orderObj[num] = index;
+  }
+})
+
+arr.forEach(num => {
+  result.push(orderObj[num]);
+})
+
+console.log(result.join(" "));
