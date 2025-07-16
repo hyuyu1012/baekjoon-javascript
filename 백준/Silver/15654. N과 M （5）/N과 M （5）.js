a390806be4ab69dcdad2c2d0ed88585c7b5b360a
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-07-16 19:42:13 +0900

    [Silver III] Title: N과 M (5), Time: 256 ms, Memory: 22680 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.js
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -1,28 +1,26 @@
 const fs = require('fs');
 const init = fs.readFileSync(0).toString().trim().split("\n");
 
-const result = [];
 const [N,M] = init[0].split(" ").map(Number);
 const array = init[1].split(" ").map(Number).sort((a,b) => a-b);
 const visited = new Array(N).fill(false);
-
+const result = [];
 
 const dfs = (n, arr) => {
   if(n === M) {
     result.push(arr.join(" "));
     return;
   }
-  for(let i = 0; i < N; i++) {
+
+  for(let i= 0; i< N; i++) {
     if(visited[i] === false) {
-      visited[i] = true;
       arr.push(array[i]);
+      visited[i] = true;
       dfs(n+1, arr);
       arr.pop();
-      visited[i] = false;
+      visited[i] = false;     
     }
   }
-
 }
-
 dfs(0, []);
 console.log(result.join("\n"));
