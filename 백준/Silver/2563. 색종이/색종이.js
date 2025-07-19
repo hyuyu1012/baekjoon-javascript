6503e65e3cd2de320852577dd1013f1731302164
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-07-19 17:52:09 +0900

    [Silver V] Title: 색종이, Time: 92 ms, Memory: 9692 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const paper = new Array(100).fill(undefined).map((element) => new Array(100).fill(false));
+
+const n = Number(input[0]);
+
+for(let i = 1; i <=n; i++) {
+  const temp = input[i].split(" ").map(Number);
+  for(let j = 0; j < 10; j++) {
+    for(let k = 0; k < 10; k++) {
+      if(paper[temp[1] + j][temp[0] + k] === false) {
+        paper[temp[1] + j][temp[0] + k]  = true;   
+      }
+    }
+  }
+}
+
+let count = 0;
+for(let i = 0; i < 100; i++) {
+  for(let j = 0; j < 100; j++) {
+    if(paper[i][j] === true) {
+      count+=1;
+    }
+  }
+}
+
+
+console.log(count);
