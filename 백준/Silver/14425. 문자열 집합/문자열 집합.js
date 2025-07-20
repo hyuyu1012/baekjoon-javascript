188159619d5d345936816c1038a0b3269456e94f
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-07-20 21:12:10 +0900

    [Silver IV] Title: 문자열 집합, Time: 172 ms, Memory: 37856 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input  = fs.readFileSync(0).toString().trim().split("\n");
+
+const [N,M] = input[0].split(" ").map(Number);
+
+const obj = Object.create(null);
+
+for(let i = 1; i<=N; i++) {
+  obj[input[i]] = true;
+}
+
+let count = 0;
+for(let i = N+1; i <= input.length-1; i++) {
+  if(input[i] in obj) {
+    count +=1;
+  }
+}
+
+console.log(count);
