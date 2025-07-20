4450825c05f22052c556df5a11194d1afa2a9374
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-07-20 22:13:32 +0900

    [Silver III] Title: 1로 만들기, Time: 340 ms, Memory: 21556 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+const dp = new Array(n+1).fill(0);
+
+dp[0] = 0;
+dp[1] = 0;
+
+for (let i = 2; i <=n; i++) {
+  const temp = [];
+  
+  if(i%3 === 0) {
+    temp.push(dp[i/3] + 1);
+  }
+  if(i%2 ===0 ) {
+    temp.push(dp[i/2] + 1);
+  }
+  temp.push(dp[i-1] + 1);
+  
+  dp[i] = Math.min(...temp);
+}
+
+console.log(dp[n]);
+
