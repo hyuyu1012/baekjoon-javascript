e524908911d6872da08e39a9613dad8a4d7ebe88
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-07-24 19:47:39 +0900

    [Silver III] Title: 파도반 수열, Time: 92 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+
+const result = [];
+const dp = [];
+const t = input[0];
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 1;
+
+for(let i =3; i<=100; i++) {
+  dp[i] = dp[i-2] + dp[i-3];
+}
+
+for(let i = 1; i <=t; i++) {
+  result.push(dp[input[i]]);  
+}
+
+console.log(result.join("\n"))
