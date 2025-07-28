e327c4fe2dead974fab2eddfc74163dbaf7869d1
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-07-28 15:35:54 +0900

    [Silver III] Title: Four Squares, Time: 180 ms, Memory: 10836 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+const dp = [];
+dp[0] = 0;
+dp[1] = 1;
+
+for(let i = 2; i <=n; i++) {
+  let min = 5;
+  for(let j = 1; j*j <=i; j++) {
+    const temp = i - j*j;
+    min = Math.min(min, dp[temp]);
+  }
+  dp[i] = min + 1;
+}
+
+console.log(dp[n]);
