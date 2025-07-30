87b7d904ed493538e15a9284a66c7c7303cd7b21
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-07-30 09:36:06 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 156 ms, Memory: 9684 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+
+const dp = [];
+dp[0] = 1;
+
+for(let i = 1; i < n; i++) {
+  let temp = 0;
+  for(let j = i -1 ; j>=0; j--) {
+    if(arr[j] < arr[i] && dp[j] > temp)  {
+      temp = dp[j];
+    }
+  }
+  dp[i] = temp + 1;
+}
+
+console.log(Math.max(...dp));
