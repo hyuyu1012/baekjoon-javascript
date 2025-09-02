ae99531ad4e04a1fe8d14902e0dbdab74d96816d
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-09-02 19:11:59 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 676 ms, Memory: 92920 KB -BaekjoonHub

--- 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.js
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.js
@@ -1,50 +1,32 @@
-// 알고리즘 탐색 - 깊이 우선 탐색 1(방문 순서 출력하기, 방문하지 못한다면 0)
-
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split("\n");
-// N : 정점의 수, M: 간선의 개수, R : 시작 정점
-const [N,M,R] = input[0].split(" ").map(Number);
+const[N,M,R] = input[0].split(" ").map(Number);
+const visited = new Array(N+1).fill(-1);
+const answer = [];
 
-const dfs = (R, adjList) => {
-  const stack = [];
-  const visited  = new Array(N+1).fill(0);
-  const depthList = new Array(N+1).fill(-1);
-  let count = 1; 
-  stack.push([R,-1]);
-
-  const result = [];
-  while(stack.length !==0) {
-    const [node,prevDepth] = stack.pop();
-    if(visited[node] !== 0) {
-      continue;
+function bfs(V, depth) {
+  visited[V] = depth;
+  for(let i = 0; i < graph[V].length; i++) {
+    if(visited[graph[V][i]] === -1){
+      bfs(graph[V][i], depth+1);
     }
-    depthList[node] = prevDepth + 1
-    visited[node] = count;
-    count++;
-    
-    adjList[node].forEach(num => {
-      if(visited[num] === 0) {
-        stack.push([num, prevDepth + 1]);
-      }
-    })
   }
-  for(let i = 1; i <= depthList.length -1; i++) {
-    result.push(depthList[i]);
-  }
-  console.log(result.join("\n"));
- }
-
-
-const adjList = Array.from({length : N+1}, () => []);
-for(let i = 1; i<=M; i++) {
-  const temp = input[i].split(" ").map(Number);
-  adjList[temp[0]].push(temp[1]);
-  adjList[temp[1]].push(temp[0]);
 }
 
-// 정렬
-for(let key in adjList) {
-  adjList[key].sort((a,b) => b-a);
+const graph = Array.from({length : N + 1}, () => []);
+
+for(let i =1; i <=M; i++) {
+  const[x,y] = input[i].split(" ").map(Number);
+  graph[x].push(y);
+  graph[y].push(x);
+} 
+
+for(let i = 0; i < graph.length; i++) {
+  graph[i].sort((a,b) => a-b);
 }
 
-dfs(R, adjList);
+bfs(R, 0);
+for(let i = 1; i < visited.length; i++) {
+  answer.push(visited[i]);
+}
+console.log(answer.join("\n"));
