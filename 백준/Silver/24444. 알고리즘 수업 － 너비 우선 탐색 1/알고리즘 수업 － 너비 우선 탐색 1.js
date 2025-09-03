b6cb52caefe82f3d8a61c50bc940d758667f9936
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-09-03 16:25:02 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 3580 ms, Memory: 95092 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const answer = [];
+
+function bfs(V) {
+  const queue = [];
+  queue.push(V);
+  let count = 1;
+  const visited = new Array(N+1).fill(0);
+  while(queue.length > 0) {
+    const node = queue.shift();
+    if(visited[node]) continue;
+    visited[node] = count;
+    count++;
+    for(let i = 0; i < graph[node].length; i++) {
+      if(!visited[graph[node][i]]) queue.push(graph[node][i]);
+    }
+  }
+  for(let i=1; i<visited.length; i++) {
+    answer.push(visited[i]);
+  }
+  console.log(answer.join("\n"))
+}
+
+const [N,M,R] = input[0].split(" ").map(Number);
+const graph = Array.from({length : N+1}, () => []);
+for(let i = 1; i <=M; i++) {
+  const[x,y] = input[i].split(" ").map(Number);
+  graph[x].push(y);
+  graph[y].push(x);
+}
+for(let i = 1; i < graph.length; i++) {
+  graph[i].sort((a,b) => a-b);
+}
+
+bfs(R);
