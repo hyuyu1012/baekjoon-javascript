b1db8874196ae78a2a41196646eeda6552527da9
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-09-05 13:28:05 +0900

    [Silver V] Title: 막대기, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.js
+++ 백준/Silver/1094. 막대기/막대기.js
@@ -1,24 +1,17 @@
-const fs = require('fs')
-const x = Number(fs.readFileSync(0).toString().trim());
-const arr = [64];
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString().trim());
 
-if(x === 64) {
-  console.log(1);
-  return;
+let index = 0;
+const array = [64, 32, 16, 8, 4, 2, 1];
+let count = 0;
+
+while(n !== 0) {
+  if(n < array[index]) {
+    index++;
+    continue;
+  }
+  n = n - array[index];
+  count++;
 }
 
-while(true) {
-  const temp = arr.pop();
-  arr.push(temp/2, temp/2);
-  let sum = 0;
-  for(let i = 0; i < arr.length -1; i++) {
-    sum = sum + arr[i];
-  }
-
-  if(sum < x) continue;
-  else if (sum > x) arr.pop();
-  else if(sum === x) {
-    console.log(arr.length -1)
-    break;
-  }
-}
+console.log(count);
