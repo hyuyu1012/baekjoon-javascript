8af5c9c8bffce9ed0cd2efb4933bb882401565b4
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-09-05 12:31:36 +0900

    [Silver V] Title: 배열 합치기, Time: 1572 ms, Memory: 406920 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.js
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.js
@@ -1,57 +1,5 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
-class Queue {
-  constructor() {
-    this.data = []; 
-    this.head = 0;
-    this.tail = 0;
-  }
-  push(data) {
-    this.data.push(data);
-  }
-  shift() {
-    return this.data[this.head++];
-  }
-  get length() {
-    return this.data.length - this.head;
-  }
-  get front() {
-    return this.data[this.head];
-  }
-}
-
-const [N,M] = input[0].split(" ").map(Number);
-const temp1 = input[1].split(" ").map(Number);
-const temp2 = input[2].split(" ").map(Number);
-const firstArray = new Queue();
-const secondArray = new Queue();
-const answer = [];
-temp1.forEach((n) => firstArray.push(n));
-temp2.forEach((n) => secondArray.push(n));
-
-while(true) {
-  if(firstArray.length === 0 && secondArray.length ===0) break;
-  if(firstArray.length ===0 ) {
-    while(secondArray.length != 0) {
-      answer.push(secondArray.shift());
-    }
-    break;
-  }
-  if(secondArray.length ===0) {
-  while(firstArray.length !== 0) {
-    answer.push(firstArray.shift());
-  }
-  break;
-  }
-
-
-  if(firstArray.front<= secondArray.front) {
-    answer.push(firstArray.shift());
-  }
-  else {
-    answer.push(secondArray.shift());
-  }
-}
-
-console.log(answer.join(" "));
+const arr = input[1].split(" ").map(Number).concat(input[2].split(" ").map(Number)).sort((a,b) => a-b);
+console.log(arr.join(" "));
