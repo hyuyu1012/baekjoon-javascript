50619c1ebec7d0def43a3637f7b53d9c01bdc75c
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-09-08 16:59:53 +0900

    [Silver I] Title: 숨바꼭질, Time: 172 ms, Memory: 18032 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,45 +1,33 @@
 const fs = require('fs');
-const [n,k] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
-const maxRange = Math.max(n,k);
+const [n, k] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-const graph = new Array(maxRange * 2 + 1).fill(Infinity);
-
+const LIMIT = 100001;
+const graph = new Array(LIMIT).fill(Infinity);
 
 class Queue {
   constructor() {
     this.data = [];
     this.head = 0;
   }
-  push(data) {
-    this.data.push(data);
-  }
-  shift() {
-    return this.data[this.head++];
-  }
-  
-  get length() {
-    return this.data.length - this.head;  
-  }
+  push(x) { this.data.push(x); }
+  shift() { return this.data[this.head++]; }
+  get length() { return this.data.length - this.head; }
 }
 
-graph[n] = 0;
 const queue = new Queue();
+graph[n] = 0;
 queue.push(n);
 
-while(graph[k] === Infinity) {  
-  const temp = queue.shift();
-  if(graph[temp -1] === Infinity) {
-    graph[temp - 1] = graph[temp] + 1;
-    queue.push(temp-1);
+while(queue.length > 0) {
+  const cur = queue.shift();
+  if(cur === k) {
+    console.log(graph[cur]);
+    break;
   }
-  if(graph[temp + 1] === Infinity) {
-    graph[temp + 1] = graph[temp] + 1;
-    queue.push(temp+1);
-  }
-  if(graph[temp * 2] === Infinity) {
-    graph[temp * 2] = graph[temp] + 1;
-    queue.push(temp * 2);
+  for(const next of [cur - 1, cur + 1, cur * 2]) {
+    if(next >= 0 && next < LIMIT && graph[next] === Infinity) {
+      graph[next] = graph[cur] + 1;
+      queue.push(next);
+    }
   }
 }
-
-console.log(graph[k]);
