ea245ca3f5f9181e261db683d25a69c33b8e7b6d
han hyungu <165983763+hyuyu1012@users.noreply.github.com>
2025-09-30 08:33:33 +0900

    [Silver IV] Title: 크림 파스타, Time: 292 ms, Memory: 56864 KB -BaekjoonHub

---
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const array = input[1].split(" ").map(Number);
+
+let max = array[0];
+let min = array[0];
+
+const dp = [];
+dp[0] = 0;
+
+for(let i = 1; i < n; i++) {
+  const temp = array[i];
+  dp[i] = Math.max(temp-max, temp-min, dp[i-1]);
+  if(temp > max) max = temp;
+  if(min > temp) min = temp;
+}
+
+console.log(dp.join(" "));
